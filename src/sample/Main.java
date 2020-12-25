ccc3c2702dd73867b7f03f30fb592826a4ac66f6
Ferrorr <61735214+Ferrorr@users.noreply.github.com>
2020-12-25 17:43:59 +0100

    działają przyciski

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Brum Brum..");
-        primaryStage.setScene(new Scene(root, 1000, 560));
+        primaryStage.setScene(new Scene(root, 1000, 580));
         primaryStage.show();
     }
 
